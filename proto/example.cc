60a186f2adeb0e94cb9268dd5d65b095b4aa345a
W. Trevor King <wking@tremily.us>
2015-09-26 22:33:50 -0700

    proto: Add example.cpp to get working JSON serialization
    In a separate branch where I tried to handle Any in Go, I got [1]:
    
      go run ./example.go
      go/config.pb.go:26:8: cannot find package "google/protobuf" in any of:
              /usr/lib/go/src/google/protobuf (from $GOROOT)
              /home/wking/.local/lib/go/src/google/protobuf (from $GOPATH)
      Makefile:31: recipe for target 'example' failed
    
    This commit switches the main example to C++, because it's protobuf's
    implementation language, so new features like Any support tend to land
    there first.
    
    The example tries to round-trip source JSON into a protobuf message
    and back.  It does that by reading from config.json (I've skipped
    runtime.json for now) into a message, and then writing JSON serialized
    from that message to stdout.  Attributes in the input JSON file that
    aren't represented in the protobuf message structure seem to be
    silently dropped (which makes forward compatibility somewhat easier,
    but explicit validation somewhat harder ;).
    
    The docs for this sort of thing seem sparse (although there is a stale
    C++ Any example here [2]).  The source skeleton [3] and Makefile rule
    [4] are based on the protobuf examples.  kTypeUrlPrefix [5],
    GetTypeUrl [6], the resolver handling [7,8], and the basic to/from
    JSON conversion [9].
    
    The commented-out LinuxUser section was how I figured out what to put
    in the process.user block of config.json.  The bits of that that
    weren't in [10], I figured out just by reading proto/cpp/config.pb.h.
    
    The Makefile rule should likely be using:
    
       $(filter %.cc, $(CPP_FILES))
    
    but my more restrictive filter removes runtime_config.pb.cc to avoid:
    
      $ make example_cpp
      c++ example.cc ./cpp/config.pb.cc ./cpp/runtime_config.pb.cc -o example_cpp $(pkg-config --cflags --libs protobuf) -I ./cpp
      ./cpp/runtime_config.pb.h:647:30: error: expected unqualified-id before numeric constant
         const ::oci::LinuxRuntime& linux() const;
                                    ^
      ...
    
    [1]: https://github.com/vbatts/specs/pull/2
    [2]: https://developers.google.com/protocol-buffers/docs/proto3#any
         Although it recommends:
           status.add_details()->PackFrom(details);
         And the actual usage would be:
           status.mutable_details()->PackFrom(details);
         See [10].
    [3]: https://github.com/google/protobuf/blob/v3.0.0-beta-1/examples/add_person.cc
    [4]: https://github.com/google/protobuf/blob/v3.0.0-beta-1/examples/Makefile#L24-L26
    [5]: https://github.com/google/protobuf/blob/v3.0.0-beta-1/src/google/protobuf/util/json_util_test.cc#L52
    [6]: https://github.com/google/protobuf/blob/v3.0.0-beta-1/src/google/protobuf/util/json_util_test.cc#L54-L56
    [7]: https://github.com/google/protobuf/blob/v3.0.0-beta-1/src/google/protobuf/util/json_util_test.cc#L66-L67
    [8]: https://github.com/google/protobuf/blob/v3.0.0-beta-1/src/google/protobuf/util/json_util_test.cc#L85
    [9]: https://github.com/google/protobuf/blob/v3.0.0-beta-1/src/google/protobuf/util/json_util_test.cc#L70-L83
    [10]: https://github.com/google/protobuf/blob/v3.0.0-beta-1/src/google/protobuf/any_test.cc#L42
    
    Signed-off-by: W. Trevor King <wking@tremily.us>

---
+++ proto/example.cc
@@ -0,0 +1,92 @@
+// See README.txt for information and build instructions.
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/util/json_util.h>
+#include <google/protobuf/util/type_resolver.h>
+#include <google/protobuf/util/type_resolver_util.h>
+
+#include "config.pb.h"
+
+using namespace std;
+
+static const char kTypeUrlPrefix[] = "type.googleapis.com";
+
+static string GetTypeUrl(const google::protobuf::Descriptor* message) {
+  return string(kTypeUrlPrefix) + "/" + message->full_name();
+}
+
+static bool ReadMessage(string path, google::protobuf::Message *message) {
+  string binary;
+  ifstream input(path.c_str(), ios::in | ios::binary);
+  if (!input) {
+    cout << path << ": File not found." << endl;
+    return false;
+  }
+  string json( (istreambuf_iterator<char>(input)),
+               istreambuf_iterator<char>() );
+  google::protobuf::scoped_ptr<google::protobuf::util::TypeResolver> resolver;
+  resolver.reset(google::protobuf::util::NewTypeResolverForDescriptorPool(
+    kTypeUrlPrefix,
+    google::protobuf::DescriptorPool::generated_pool()));
+  GOOGLE_CHECK_OK(google::protobuf::util::JsonToBinaryString(
+    resolver.get(),
+    GetTypeUrl(message->GetDescriptor()),
+    json,
+    &binary));
+  return message->ParseFromString(binary);
+}
+
+static bool WriteMessage(const google::protobuf::Message& message) {
+  string json;
+  google::protobuf::util::JsonOptions options;
+  google::protobuf::scoped_ptr<google::protobuf::util::TypeResolver> resolver;
+  resolver.reset(google::protobuf::util::NewTypeResolverForDescriptorPool(
+    kTypeUrlPrefix,
+    google::protobuf::DescriptorPool::generated_pool()));
+  options.add_whitespace = true;
+  GOOGLE_CHECK_OK(google::protobuf::util::BinaryToJsonString(
+    resolver.get(),
+    GetTypeUrl(message.GetDescriptor()),
+    message.SerializeAsString(),
+    &json,
+    options));
+  cout << json;
+  return true;
+}
+
+// Main function: Reads the config from a file and writes it to stdout.
+int main(int argc, char* argv[]) {
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  oci::Spec config;
+
+  if (!ReadMessage("config.json", &config)) {
+    cerr << "config.json: Failed to load." << endl;
+    return -1;
+  }
+
+/*
+  oci::LinuxUser user;
+  user.set_uid(1);
+  user.set_gid(1);
+  user.add_additional_gids(5);
+  user.add_additional_gids(6);
+  config.mutable_process()->mutable_user()->PackFrom(user);
+*/
+
+  if (!WriteMessage(config)) {
+    cerr << "config.json: Failed to write to stdout." << endl;
+    return -1;
+  }
+
+  // Optional:  Delete all global objects allocated by libprotobuf.
+  google::protobuf::ShutdownProtobufLibrary();
+
+  return 0;
+}
